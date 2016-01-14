ae69e86a13786d964a2f7bcc1292c939d6779cc5
mvanevery <mvanevery@madmobile.com>
2016-01-14 10:52:09 -0500

    Added in functionality to set the browser userAgent to a mobile device. In hoped to actually emulate a mobile device.

--- test.js
+++ test.js
@@ -1,9 +1,18 @@
 var webdriverio = require('webdriverio');
 var options = {
 	desiredCapabilities: {
-		browserName: 'firefox'
+		browserName: 'phantomjs'
 	}
 };
+
+options.desiredCapabilities.phantomjs = {
+	page: {
+		settings: {
+			userAgent: 'Mozilla/5.0 (Linux; Android 4.4; Nexus 5 Build/BuildID) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/30.0.0.0 Mobile Safari/537.36'
+		}
+	}
+};
+
 webdriverio
 	.remote(options)
 	.init()
@@ -11,4 +20,10 @@ webdriverio
 	.title(function(err, res) {
 		console.log(res.value);
 	})
-	.end();
+	.onloadend = function(screen){
+		screen.saveScreenshot('c:/screen.png')
+	}
+	.end;
+
+
+

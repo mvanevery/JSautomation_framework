06d405fac81116fe54af738c37b06cbabcfd5b47
derickwarshaw <dwarshaw@madmobile.com>
2016-02-18 06:32:11 -0500

    notes

--- setup.js
+++ setup.js
@@ -1,5 +1,8 @@
 var selenium = require('selenium-standalone');
 
+// Think about using environment variables to differentiate bamboo from local so bamboo points to that global install
+// Weird that selenium.start will run but one inside the child process is undefined
+
 selenium.start(function(err, child) {
 
 	child.stderr.on('data', function(data){

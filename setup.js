c87b459f8776cf1801eb78c2b1f6a39e2ae3b065
derickwarshaw <dwarshaw@madmobile.com>
2016-02-18 17:21:44 -0500

    logging err

--- setup.js
+++ setup.js
@@ -4,7 +4,7 @@ var selenium = require('selenium-standalone');
 // Weird that selenium.start will run but one inside the child process is undefined
 
 selenium.start(function(err, child) {
-
+	console.log(err);
 	child.stderr.on('data', function(data){
 		console.log(data.toString());
 	});

1f4c70b69d493a6172e9cc45aa7a386ba1f9fe60
mvanevery <mvanevery@madmobile.com>
2016-02-23 12:35:21 -0500

    resetting head

--- core/client.js
+++ core/client.js
@@ -3,7 +3,7 @@ var config = require('./projects/payless/config');
 exports.client = require('webdriverio').remote({
 	desiredCapabilities: {
 		browserName: 'phantomjs',
-		'phantomjs.binary.path': require('phantomjs').path,
+		//'phantomjs.binary.path': require('phantomjs').path,
 		page: {
 			settings: {
 				userAgent: config.userAgents.mobile

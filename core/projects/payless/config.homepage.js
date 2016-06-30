37b68cb75285e81183d3508f8c4f22600b15fb89
derickwarshaw <dwarshaw@madmobile.com>
2016-06-30 14:04:46 -0400

    deleting things that I doubt are being used

--- core/projects/payless/config.homepage.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  mobileTitle: 'Payless ShoeSource | Shoes, Boots, Sandals, Designer Shoes & Handbags',
-  tabletTitle: 'Payless Tablet',
-  mobileUrl: 'https://m.payless.com/',
-  tabletUrl: 'https://t.payless.com/'
-};

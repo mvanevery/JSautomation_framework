f71f3074c8bd576c949b774edb6708d438ed9707
derickwarshaw <dwarshaw@madmobile.com>
2016-07-05 08:04:48 -0400

    improving gulp file but it still needs work

--- gulpfile.js
+++ gulpfile.js
@@ -9,26 +9,15 @@ const gulp = require('gulp');
 const browserSync = require('browser-sync');
 const selenium = require('selenium-standalone');
 const mocha = require('gulp-mocha');
+const project = require('./core/projects/config').project;
 
-const mochaOptions = {
-  timeout: '50000',
-  reporter: 'mochawesome',
-  reporterOptions: {
-    reportDir: 'test',
-    reportName: 'report',
-    reportTitle: 'awesome',
-    inlineAssets: true
-  }
+const getTask = function (task) {
+   return require(`./gulp-tasks/${project}/${task}`)(gulp, mocha);
 };
 
 function terminateProcess() {
   selenium.child.kill();
   browserSync.exit();
-}
-
-function handleError(err) {
-  console.log(err.toString());
-  this.emit('end');
 }
 
 gulp.task('serve:test', function (done) {
@@ -75,46 +64,49 @@ gulp.task('selenium-start', function (done) {
   });
 });
 
-gulp.task('e2e-guest', ['serve:test', 'selenium'], function () {
-  return gulp.src('test/payless/staging/E2E-guest.js', {read: false})
-    .pipe(mocha(mochaOptions).on("error", handleError));
-});
-gulp.task('e2e-prod', ['serve:test', 'selenium'], function () {
-  return gulp.src('test/payless/production/end2end-prod.js', {read: false})
-    .pipe(mocha(mochaOptions).on("error", handleError));
-});
-gulp.task('e2e-return', ['serve:test', 'selenium'], function () {
-  return gulp.src('test/payless/staging/E2E-returning-user.js', {read: false})
-    .pipe(mocha(mochaOptions).on("error", handleError));
-});
-gulp.task('findItem', ['serve:test', 'selenium'], function () {
-  return gulp.src('test/payless/staging/findProduct.js', {read: false})
-    .pipe(mocha(mochaOptions).on("error", handleError));
-});
-gulp.task('findStore', ['serve:test', 'selenium'], function () {
-  return gulp.src('test/payless/staging/findStore.js', {read: false})
-    .pipe(mocha(mochaOptions).on("error", handleError));
-});
-gulp.task('findInStore', ['serve:test', 'selenium'], function () {
-  return gulp.src('test/payless/staging/findInStore.js', {read: false})
-    .pipe(mocha(mochaOptions).on("error", handleError));
+gulp.task('findInStore', ['serve:test', 'selenium'], getTask('findInStore'));
+gulp.task('findStore', ['serve:test', 'selenium'], getTask('findStore'));
+gulp.task('findItem', ['serve:test', 'selenium'], getTask('findItem'));
+gulp.task('e2e-guest', ['serve:test', 'selenium'], getTask('e2e-guest'));
+gulp.task('e2e-prod', ['serve:test', 'selenium'], getTask('e2e-prod'));
+gulp.task('e2e-return', ['serve:test', 'selenium'], getTask('e2e-return'));
+
+gulp.task('initiate-find-in-store', ['findInStore'], function () {
+  terminateProcess();
 });
 
-gulp.task('find-a-store', ['findStore'], function () {
+gulp.task('init-find-store', ['findInStore'], function () {
   terminateProcess();
 });
-gulp.task('find-item', ['findItem'], function () {
+
+gulp.task('init-find-item', ['findItem'], function () {
   terminateProcess();
 });
-gulp.task('find-in-store', ['findInStore'], function () {
+
+gulp.task('init-find-a-store', ['findStore'], function () {
   terminateProcess();
 });
-gulp.task('e2e-return-user', ['e2e-return'], function () {
+
+gulp.task('init-find-item', ['findItem'], function () {
   terminateProcess();
 });
-gulp.task('e2e-guest-user', ['e2e-guest'], function () {
+
+gulp.task('init-find-in-store', ['findInStore'], function () {
   terminateProcess();
 });
-gulp.task('e2e-prod-test', ['e2e-prod'], function () {
+
+gulp.task('init-e2e-return-user', ['e2e-return'], function () {
+  terminateProcess();
+});
+
+gulp.task('init-e2e-guest-user', ['e2e-guest'], function () {
+  terminateProcess();
+});
+
+gulp.task('init-e2e-prod-test', ['e2e-prod'], function () {
+  terminateProcess();
+});
+
+gulp.task('init-e2e-return', ['e2e-return'], function () {
   terminateProcess();
 });

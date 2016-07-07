498b0f1997cc871d44a776b5476f15f5f12af09e
derickwarshaw <dwarshaw@madmobile.com>
2016-07-06 23:14:12 -0400

    updated for dynamic client

--- core/projects/config.js
+++ core/projects/config.js
@@ -6,10 +6,16 @@ const test = process.argv.filter((argument) => {
   return argument.match(/--test/);
 });
 
+const client = process.argv.filter((argument) => {
+  return argument.match(/--client/);
+});
+
 const projectIndex = process.argv.indexOf(project[0]);
 const testIndex = process.argv.indexOf(test[0]);
+const clientIndex = process.argv.indexOf(client[0]);
 
 module.exports = {
   project: !!project[0] ? process.argv[projectIndex].replace(/--archon:/, '') : 'rutt roh',
-  test: !!test[0] ? process.argv[testIndex].replace(/--test:/, '') : 'no test defined'
+  test: !!test[0] ? process.argv[testIndex].replace(/--test:/, '') : 'no test defined',
+  client: !!client[0] ? process.argv[clientIndex].replace(/--client:/, '') : 'phantomjs'
 };

2ff602c811dcb30d497086df5c1603aff7281b42
mvanevery <mvanevery@madmobile.com>
2016-09-08 15:28:17 -0400

    updating with new concierge names and steps

--- src/test/concierge/loginSSales.js
+++ src/test/concierge/loginSSales.js
@@ -48,13 +48,13 @@ describe('Smoke Test: Login as "ssales"', () => {
     });
   });
 
-  //describe('Verify Concierge Screen', () => {
-  //  it('Should navigate to the Concierge screen', (done) => {
-  //    common.verifyConciergeScreen(done);
-  //  });
-  //});
+  describe('Verify Concierge Screen', () => {
+    it('Should navigate to the Concierge screen', (done) => {
+      common.verifyConciergeScreen(done);
+    });
+  });
 
-  //after((done) => {
-  //  common.end(done);
-  //});
+  after((done) => {
+    common.end(done);
+  });
 });

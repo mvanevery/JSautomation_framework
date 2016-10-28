eb396694472d143823d48150f87500b70813c5c2
afeldmeyer <afeldmeyer@madmobile.com>
2016-10-28 13:26:21 -0400

    added intial test setup for BAM

--- src/core/projects/concierge-BAM/selectors/loginPage.js
+++ src/core/projects/concierge-BAM/selectors/loginPage.js
@@ -6,11 +6,11 @@ module.exports = {
       baseUrl: 'https://serviceqa.booksamillion.com/'
     },
     helpers: {
-      img_loginLogo:           '//div[@class="login-form__logo"]',
+      img_loginLogo:        '//div[@class="login-form__logo"]',
       fld_username:         '//input[@name="employee_id"]',
       fld_password:         '//input[@name="employee_pin"]',
       fld_storeOverride:    '//input[@name="store_override"]',
-      btn_signIn:           '//button[@type="submit"]'
+      btn_signIn:           '//button[@type="submit"]',
 
       data_username:        '180001',
       data_password:        '1234'

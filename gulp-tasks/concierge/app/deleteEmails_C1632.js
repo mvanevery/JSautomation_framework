0b4232f973f53eaa9bf48c880ebb13f5cfdc1250
John Harre <jharre@madmobile.com>
2016-11-09 14:20:46 -0500

    added deleteEmails to gulp-tasks

---
+++ gulp-tasks/concierge/app/deleteEmails_C1632.js
@@ -0,0 +1,26 @@
+/**
+ * Created by jharre on 11/9/16.
+ */
+const mochaOptions = {
+  timeout: '50000',
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: './src/test/concierge/reports',
+    reportName: 'report',
+    reportTitle: 'Add Appointment through Widget',
+    inlineAssets: true
+  }
+};
+const projectType = require('../../../src/core/projects/config').projectType;
+
+function handleError(err) {
+  console.log(err.toString());
+  this.emit('end');
+}
+
+module.exports = function (gulp, mocha) {
+  return function () {
+    return gulp.src('./src/test/concierge/appSuite/Email/replyingToAThread_C1639.js', {read: false})
+      .pipe(mocha(mochaOptions).on("error", handleError));
+  }
+};

28a7f4ef0834849b8f740d0ee6037e0dc68800c5
mvanevery <mvanevery@madmobile.com>
2016-11-16 15:30:05 -0500

    updated provisioning key for Talbots

--- src/core/projects/concierge/selectors/provisioning.js
+++ src/core/projects/concierge/selectors/provisioning.js
@@ -10,7 +10,7 @@ module.exports = {
         concierge_logo: '//img[@alt="Concierge"]',
         keyField: '//input[@name="authKey"]',
         burberry_provkey: '4e044de8-0b50-46c1-d76d-46f3a4df4b86',
-        talbots_provkey: 'https://manage.stage.madcloud.io/api/v1/discovery/cc20a9a0-423b-d6ea-3aab-ba20370c8fd9',
+        talbots_provkey: 'https://manage.madcloud.io/api/v1/discovery/526f2d23-698f-93e3-0645-16d149175405',
         submitButton: '//button[@type="button"]',
         placeholderText: 'Enter provision code'
     }

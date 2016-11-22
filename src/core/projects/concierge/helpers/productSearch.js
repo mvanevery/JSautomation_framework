e568da8de92dc9caf1900fc7b71dae748ebf5ce8
mvanevery <mvanevery@madmobile.com>
2016-11-22 16:21:02 -0500

    adding product search stuff

--- src/core/projects/concierge/helpers/productSearch.js
+++ src/core/projects/concierge/helpers/productSearch.js
@@ -1,3 +1,32 @@
-/**
- * Created by mvanevery on 11/16/16.
- */
+const clientType = require('../../../projects/config').client;
+const client = require(`../../../../core/clients/${clientType}`).client;
+const project = require('../../../projects/config').project;
+const config = require(`../../../projects/${project}/config`);
+const landingPage = require(`../../../projects/${project}/selectors/landingPage`);
+const loginPage = require(`../../../projects/${project}/selectors/loginPage`);
+const provisioning = require(`../../../projects/${project}/selectors/provisioning`);
+const store = require(`../../../projects/${project}/selectors/store`);
+const planner = require(`../../../projects/${project}/selectors/planner`);
+const blackbook = require(`../../../projects/${project}/selectors/blackbook`);
+const assert = require('chai').assert;
+const expect = require('chai').expect;
+const $ = require('chai-Jquery');
+
+
+module.exports = {
+
+  verifySearchOpen(done, expected) {
+    if (client.isVisible(landingPage.helpers.productSearchField, done)) {
+          client.getText(landingPage.helpers.productSearchField)
+            .then((text) => {
+              try {
+                assert.equal(expected, text, 'The expected value was not equal to the text');
+              } catch (err) {
+                done(err);
+              }
+            })
+          }
+        },
+
+
+}

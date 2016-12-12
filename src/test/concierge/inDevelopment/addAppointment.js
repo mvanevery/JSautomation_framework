7dcfc2e73a035427358a05f7e755dbd94afdd397
qa <archon.jenkins@madmobile.com>
2016-12-12 17:36:41 -0500

    Merge branch 'develop' of bitbucket.org:madmobile/archon-framework into feature/IDP-359-c1665-select-variant-combination
    # Conflicts:
    #	src/test/concierge/appSuite/Catalog/selectVariantCombination_C1665.js

--- src/test/concierge/inDevelopment/addAppointment.js
+++ src/test/concierge/inDevelopment/addAppointment.js
@@ -6,10 +6,10 @@
  * Date:        11/1/2016
  */
 
-const common = require('../../../core/projects/concierge/helpers/common');
-const login = require('../../../core/projects/concierge/helpers/loginPage');
-const landing = require('../../../core/projects/concierge/helpers/landingPage');
-const planner = require('../../../core/projects/concierge/helpers/planner');
+const common = require('../../../../core/projects/concierge/helpers/common');
+const login = require('../../../../core/projects/concierge/helpers/loginPage');
+const landing = require('../../../../core/projects/concierge/helpers/landingPage');
+const planner = require('../../../../core/projects/concierge/helpers/planner');
 
 //let CUSTOMERNAME;
 //const setName = (name) => CUSTOMERNAME = name;
@@ -22,7 +22,7 @@ describe('Smoke Test: Add Appointment', () => {
 
   describe('Verify Login Screen', () => {
     it('Should navigate to the login screen', (done) => {
-      login.verifyLoginScreen(done, true);
+      login.verifyLoginScreen(done);
     });
   });
   describe('Pausing', () => {
@@ -33,7 +33,7 @@ describe('Smoke Test: Add Appointment', () => {
 
   describe('Login', () => {
     it('Should login as "testauto"', (done) => {
-      login.loginUser(done,true, 'testauto', 'qaUser@123');
+      login.loginUser(done, 'testauto', 'qaUser@123');
     });
   });
 
@@ -45,7 +45,7 @@ describe('Smoke Test: Add Appointment', () => {
 
   describe('Click the Planner menu option', () => {
     it('Should open planner sections', (done) => {
-      landing.navigateIcons(done, 'plannerIcon', true);
+      landing.navPlanner(done, "Planner");
     });
   });
 
@@ -55,9 +55,9 @@ describe('Smoke Test: Add Appointment', () => {
     });
   });
 
-  describe('Change the type and Open Appointment Form', () => {
+  describe('Change the type and Open Task Form', () => {
     it('Should open the appointment form from the Planner', (done) => {
-      planner.apptToggle(done, true, "Create Stuff Appointment");
+      planner.apptToggle(done, "Create Appointment");
     });
   });
   describe('Pausing', () => {
@@ -110,8 +110,8 @@ describe('Smoke Test: Add Appointment', () => {
       common.pause(done, 2000);
     });
   });
-  describe('Verify Added Appointment', () => {
-    it('Should verify that the Appointment is displayed on Planner Page', (done) => {
+  describe('Verify Added Task', () => {
+    it('Should verify that the Task is displayed on Planner Page', (done) => {
       planner.verifyAddedAppt(done, 'Automation Test Appointment');
     });
   });
@@ -122,13 +122,13 @@ describe('Smoke Test: Add Appointment', () => {
     });
   });
   describe('Logout', () => {
-    it('Should logout the current user', (done) => {
-      login.logoutUser(done, true);
+    it('Should Logout from the current user', (done) => {
+      login.logoutUser(done);
     });
   });
-  describe('Test Completed: Cleaning up', () => {
-    it('Should pause the test for 10 seconds', (done) => {
-      common.pause(done, 10000);
+  describe('Pausing', () => {
+    it('Should pause the test for 2 seconds', (done) => {
+      common.pause(done, 5000);
     });
   });
 })

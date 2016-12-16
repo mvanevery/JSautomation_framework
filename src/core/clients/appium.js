d469d9b089253783872f58e1341961a3e9332b43
mvanevery <mvanevery@madmobile.com>
2016-12-16 11:27:45 -0500

    updated the client file for appium

--- src/core/clients/appium.js
+++ src/core/clients/appium.js
@@ -6,14 +6,14 @@ exports.client = require('webdriverio').remote({
     platformName: 'iOS',
     //bundleId: 'com.mmsyscouom.app',
     bundleId: 'com.madmobiledev.ConciergeDev',
-    platformVersion: '9.3',
-    deviceName: 'Mad Mobile Mini iPad',
-    udid: '6bbbd889caeed808c71677e5bd5f1c7f764eaddd',
+    //platformVersion: '9.3',
+    //deviceName: 'Mad Mobile Mini iPad',
+    //udid: '6bbbd889caeed808c71677e5bd5f1c7f764eaddd',
     //automationName: 'XCUITest',
     autoWebview: true,
    // realDeviceLogger: '/Users/jharre/.nvm/versions/node/v6.6.0/lib/node_modules/deviceconsole/deviceconsole'
-   // platformVersion: '9.3.2',
-   // deviceName: 'MM Concierge-Silver',
-   // udid: 'dff8ad9bb716a56766d98b2232c3f541ba9bb796'
+    platformVersion: '9.3.2',
+    deviceName: 'MM Concierge-Silver',
+    udid: 'dff8ad9bb716a56766d98b2232c3f541ba9bb796'
   }
 });

1fbb5a65b62abbaa102f1fc21c520b5ce8e81efb
afeldmeyer <afeldmeyer@madmobile.com>
2016-12-16 15:40:39 -0500

    Updated test to take advantage of 'verifyURL' refactor.

--- src/test/concierge-BAM/Suite/Dashboard_Layout/04_associateResourceLinks_C282.js
+++ src/test/concierge-BAM/Suite/Dashboard_Layout/04_associateResourceLinks_C282.js
@@ -10,6 +10,7 @@
 
 const common = require('../../../../core/projects/concierge-BAM/helpers/common');
 
+
 describe('Test the Associate Resources links.', () => {
 
   describe('Pausing', () => {
@@ -18,122 +19,61 @@ describe('Test the Associate Resources links.', () => {
     });
   });
 
-  describe('Verify Login Screen', () => {
-    it('Should navigate to the login screen', (done) => {
-      common.logpinPageElementVisible(done, 'btn_signIn', true);
+  // Verify the <Associate Feedback Survey> link
+  describe('Evaluate the "Associate Feedback Survey" URL', () => {
+    it('Should verify the "Associate Feedback Survey" URL is correct', (done) => {
+      common.verifyURL(done, "btn_associateFeedback", true, "lnk_associateFeedback");
     });
   });
 
-  describe('Login', () => {
-    it('Should login as "180001"', (done) => {
-      common.loginUser(done, 180001, 1234);
+  // Verify the <Associate Magazine Center> link
+  describe('Evaluate the "Associate Magazine Center" URL', () => {
+    it('Should verify the "Associate Magazine Center" URL is correct', (done) => {
+      common.verifyURL(done, "btn_associateMagCenter", true, "lnk_associateMagCenter");
     });
   });
+
+  // Verify the <Associate Self Service> link
+  describe('Evaluate the "Associate Self Service" URL', () => {
+    it('Should verify the "Associate Self Service" URL is correct', (done) => {
+      common.verifyURL(done, "btn_associateSelfServe", true, "lnk_associateSelfServe");
+    });
+  });
+
+  // Verify the <BAM! University> link
+  describe('Evaluate the "BAM! University" URL', () => {
+    it('Should verify the "BAM! University" URL is correct', (done) => {
+      common.verifyURL(done, "btn_bamUniversity", true, "lnk_bamUniversity");
+    });
+  });
+
+  // Verify the <BAM! Company Store> link
+  describe('Evaluate the "BAM! Company Store" URL', () => {
+    it('Should verify the "BAM! Company Store" URL is correct', (done) => {
+      common.verifyURL(done, "btn_bamStore", true, "lnk_bamStore");
+    });
+  });
+
+  //  Verify the <Bright Ideas> link
+  describe('Evaluate the "Bright Ideas" URL', () => {
+    it('Should verify the "Bright Ideas" URL is correct', (done) => {
+      common.verifyURL(done, "btn_brightIdeas", true, "lnk_brightIdeas");
+    });
+  });
+
+  // Verify the <Exit Survey> link
+  describe('Evaluate the "Exit Survey" URL', () => {
+    it('Should verify the "Exit Survey" URL is correct', (done) => {
+      common.verifyURL(done, "btn_brightIdeas", true, "lnk_brightIdeas");
+    });
+  });
+
 
   describe('Pausing', () => {
-    it('Should pause the test for 15 seconds', (done) => {
-      common.pause(done, 15000);
+    it('Should pause the test for 5 seconds', (done) => {
+      common.pause(done, 5000);
     });
   });
 
-  describe('Test the Associate Resources links.', () => {
 
-    describe('Pausing', () => {
-      it('Should pause the test for 5 seconds', (done) => {
-        common.pause(done, 5000);
-      });
-    });
-
-    // Verify the <Associate Feedback Survey> link
-    describe('Evaluate "Associate Feedback Survey"', () => {
-      it('Should verify that "Associate Feedback Survey" appears within the Associate Resources section of the dashboard.', (done) => {
-        common.dashboardElementVisible(done, "btn_associateFeedback", true);
-      });
-    });
-    describe('Evaluate the "Associate Feedback Survey" URL', () => {
-      it('Should verify the "Associate Feedback Survey" URL is correct', (done) => {
-        common.dashboardElementVisible(done, "lnk_associateFeedback", true);
-      });
-    });
-
-    // Verify the <Associate Magazine Center> link
-    describe('Evaluate "Associate Magazine Center"', () => {
-      it('Should verify that "Associate Magazine Center" appears within the Associate Resources section of the dashboard.', (done) => {
-        common.dashboardElementVisible(done, "btn_associateMagCenter", true);
-      });
-    });
-    describe('Evaluate the "Associate Magazine Center" URL', () => {
-      it('Should verify the "Associate Magazine Center" URL is correct', (done) => {
-        common.dashboardElementVisible(done, "lnk_associateMagCenter", true);
-      });
-    });
-
-    // Verify the <Associate Self Service> link
-    describe('Evaluate "Associate Self Service"', () => {
-      it('Should verify that "Associate Self Service" appears within the Associate Resources section of the dashboard.', (done) => {
-        common.dashboardElementVisible(done, "btn_associateSelfServe", true);
-      });
-    });
-    describe('Evaluate the "Associate Self Service" URL', () => {
-      it('Should verify the "Associate Self Service" URL is correct', (done) => {
-        common.dashboardElementVisible(done, "lnk_associateSelfServe", true);
-      });
-    });
-
-    // Verify the <BAM! University> link
-    describe('Evaluate "BAM! University"', () => {
-      it('Should verify that "BAM! University" appears within the Associate Resources section of the dashboard.', (done) => {
-        common.dashboardElementVisible(done, "btn_bamUniversity", true);
-      });
-    });
-    describe('Evaluate the "BAM! University" URL', () => {
-      it('Should verify the "BAM! University" URL is correct', (done) => {
-        common.dashboardElementVisible(done, "lnk_bamUniversity", true);
-      });
-    });
-
-    // Verify the <BAM! Company Store> link
-    describe('Evaluate "BAM! Company Store"', () => {
-      it('Should verify that "BAM! Company Store" appears within the Associate Resources section of the dashboard.', (done) => {
-        common.dashboardElementVisible(done, "btn_bamStore", true);
-      });
-    });
-    describe('Evaluate the "BAM! Company Store" URL', () => {
-      it('Should verify the "BAM! Company Store" URL is correct', (done) => {
-        common.(done, "lnk_bamStore", true);
-      });
-    });
-
-    //  Verify the <Bright Ideas> link
-    describe('Evaluate "Bright Ideas"', () => {
-      it('Should verify that "Bright Ideas" appears within the Associate Resources section of the dashboard.', (done) => {
-        common.dashboardElementVisible(done, "btn_brightIdeas", true);
-      });
-    });
-    describe('Evaluate the "Bright Ideas" URL', () => {
-      it('Should verify the "Bright Ideas" URL is correct', (done) => {
-        common.verifyURL(done, "btn_brightIdeas", true, "https://www.surveymonkey.com/r/BAMBrightIdeas");
-      });
-    });
-
-    // Verify the <Exit Survey> link
-    describe('Evaluate "Exit Survey"', () => {
-      it('Should verify that "Exit Survey" appears within the Associate Resources section of the dashboard.', (done) => {
-        common.dashboardElementVisible(done, "btn_exitSurvey", true);
-      });
-    });
-    describe('Evaluate the "Exit Survey" URL', () => {
-      it('Should verify the "Exit Survey" URL is correct', (done) => {
-        common.dashboardElementVisible(done, "lnk_exitSurvey", true);
-      });
-    });
-
-    describe('Pausing', () => {
-      it('Should pause the test for 5 seconds', (done) => {
-        common.pause(done, 5000);
-      });
-    });
-
-
-  })
 })

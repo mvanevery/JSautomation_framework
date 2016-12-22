606fb9de03ecc72c93f5317737ea4706f03c48b7
John Harre <jharre@madmobile.com>
2016-12-22 11:01:00 -0500

    Merge branches 'develop' and 'feature/IDP-356-c192-expand-description' of https://bitbucket.org/madmobile/archon-framework into feature/IDP-356-c192-expand-description
    # Conflicts:
    #	src/core/projects/concierge/helpers/catalog.js
    #	src/core/projects/concierge/selectors/catalog.js
    #	src/test/concierge/appSuite/Catalog/defaultDescription_C191.js

--- src/test/concierge/appSuite/Catalog/expandDescription_C192.js
+++ src/test/concierge/appSuite/Catalog/expandDescription_C192.js
@@ -1,7 +1,7 @@
 /**
  * Project:     Concierge
- * TestName     Catalog/defaultDescription_C191
- * TestSteps:   https://madmobile.testrail.net/index.php?/cases/view/191
+ * TestName     Catalog/expandDescription_C192
+ * TestSteps:   https://madmobile.testrail.net/index.php?/cases/view/192
  * Author:      John Harre
  * Date:        11/30/2016
  */
@@ -11,147 +11,135 @@ const login = require('../../../../core/projects/concierge/helpers/loginPage');
 const landing = require('../../../../core/projects/concierge/helpers/landingPage');
 const catalog = require('../../../../core/projects/concierge/helpers/catalog');
 
-describe('Smoke Test: C191 Default Descriptiobn', () => {
+describe('Smoke Test: C192 Default Description.', () => {
   before((done) => {
     common.goTo(done);
   });
 
-  describe('Verify Login Screen', () => {
-    it('Should navigate to the login screen', (done) => {
+  describe('Verify Login Screen.', () => {
+    it('Should navigate to the login screen.', (done) => {
       login.verifyLoginScreen(done);
     });
   });
 
-  describe('Login', () => {
-    it('Should login as "testauto"', (done) => {
-      login.loginUser(done, 'testauto', 'qaUser@123');
+  describe('Login.', () => {
+    it('Should login as "testauto".', (done) => {
+      login.loginUser(done, true, 'testauto', 'qaUser@123');
     });
   });
 
-  describe('Pausing', () => {
-    it('Should pause the test for 10 seconds', (done) => {
+  describe('Pausing.', () => {
+    it('Should pause the test for 10 seconds.', (done) => {
       common.pause(done, 10000);
     });
   });
 
-  describe('Click the Catalog menu option', () => {
-    it('Should open Catalog section', (done) => {
-      landing.navCatalog(done, "Catalog");
+  describe('Click the product catalog icon in the left navigation bar.', () => {
+    it('Product Catalog menu displays.', (done) => {
+      landing.navigateIcons(done, "catalogMenuIcon", true);
     });
   });
 
-  describe('Pausing', () => {
-    it('Should pause the test for 5 seconds', (done) => {
+  describe('Pausing.', () => {
+    it('Should pause the test for 5 seconds.', (done) => {
       common.pause(done, 5000);
     });
   });
 
-    describe('Click the Shoes menu option', () => {
-      it('Should open Shoes section', (done) => {
-        catalog.LeftDrawer(done, "Shoes");
-      });
+  describe('Click the product catalog icon in the left navigation bar.', () => {
+  it('Product Catalog menu displays.', (done) => {
+      landing.navigateTree(done, "catalogMenuTitle", true);
     });
+  });
 
-    describe('Pausing', () => {
-      it('Should pause the test for 5 seconds', (done) => {
-        common.pause(done, 5000);
-      });
+  describe('Pausing.', () => {
+    it('Should pause the test for 5 seconds.', (done) => {
+      common.pause(done, 5000);
     });
+  });
 
-    describe('Click the Shoes -> Flats menu option', () => {
-      it('Should open Shoes -> Flats section', (done) => {
-        catalog.LeftDrawer(done, "Flats");
-      });
-    });
+  describe('Continue to go through branches of the product catalog menu tree until you click a leaf of the tree.', () => {
+     it('The PLP of that leaf category displays.', (done) => {
+       catalog.navigateDrawerProducts(done, "productCategory", true);
+     });
+   });
 
-    describe('Pausing', () => {
-      it('Should pause the test for 5 seconds', (done) => {
-        common.pause(done, 5000);
-      });
+  describe('Pausing.', () => {
+    it('Should pause the test for 5 seconds.', (done) => {
+      common.pause(done, 5000);
     });
+  });
 
-    describe('Click on a Shoes -> Flats product.', () => {
-      it('Should open Flats product details page.', (done) => {
-        catalog.openProductDetailsPage(done);
-      });
-    });
+  describe('Continue to go through branches of the product catalog menu tree until you click a leaf of the tree.', () => {
+     it('The PLP of that leaf category displays.', (done) => {
+       catalog.navigateDrawerProducts(done, "productSubCategory", true);
+     });
+   });
 
-    describe('Pausing', () => {
-      it('Should pause the test for 5 seconds', (done) => {
-        common.pause(done, 5000);
-      });
+  describe('Pausing.', () => {
+    it('Should pause the test for 5 seconds.', (done) => {
+      common.pause(done, 5000);
     });
+  });
 
-    describe('See a Shoes -> Flats product details.', () => {
-      it('Should see Shoes -> Flats product details page.', (done) => {
-        catalog.productDetailsPage(done, "Edison Flats-Roses");
-      });
-    });
+  describe('Click on one of the products on the PLP.', () => {
+     it('User is navigated to the PDP of that product.', (done) => {
+       catalog.productListPage(done, "product", true);
+     });
+   });
 
-    describe('Pausing', () => {
-      it('Should pause the test for 5 seconds', (done) => {
-        common.pause(done, 5000);
-      });
+  describe('Pausing.', () => {
+    it('Should pause the test for 5 seconds.', (done) => {
+      common.pause(done, 5000);
     });
+  });
 
-    describe('See read more... link', () => {
-      it('Should click read more... link.', (done) => {
-        catalog.readMoreLink(done);
-      });
+  describe('Click on one of the products on the PLP.', () => {
+    it('User is navigated to the PDP of that product. The description of that product is by default ellipsified and shortened. A toggle is displayed that allows the user to expand.', (done) => {
+      catalog.readMoreLink(done, "readMoreLink", true);
     });
+  });
 
-    describe('Pausing', () => {
-      it('Should pause the test for 5 seconds', (done) => {
-        common.pause(done, 5000);
-      });
+  describe('Pausing.', () => {
+    it('Should pause the test for 5 seconds.', (done) => {
+      common.pause(done, 5000);
     });
+  });
 
-    describe('See read less... link', () => {
-      it('Should see read less... link.', (done) => {
-        catalog.readLessLinkChk(done,false);
-      });
+  describe('Click the toggle to expand the description.', () => {
+    it('Full product description displays.', (done) => {
+      catalog.clickReadMoreLink(done, "readMoreLink", true);
     });
+  });
 
-    describe('Pausing', () => {
-      it('Should pause the test for 5 seconds', (done) => {
-        common.pause(done, 5000);
-      });
+  describe('Pausing.', () => {
+    it('Should pause the test for 5 seconds.', (done) => {
+      common.pause(done, 5000);
     });
+  });
 
-    describe('See read less... link', () => {
-      it('Should click read less... link.', (done) => {
-        catalog.readLessLink(done);
-      });
+  describe('Click the toggle again.', () => {
+    it('Description becomes ellipsified again.', (done) => {
+      catalog.clickReadMoreLink(done, "readMoreLink", true);
     });
+  });
 
-    describe('Pausing', () => {
-      it('Should pause the test for 5 seconds', (done) => {
-        common.pause(done, 5000);
-      });
+  describe('Pausing.', () => {
+    it('Should pause the test for 5 seconds.', (done) => {
+      common.pause(done, 5000);
     });
+  });
 
-    describe('See read more... link', () => {
-      it('Should see read more... link.', (done) => {
-        catalog.readMoreLinkChk(done,false);
-      });
+  describe('Logout.', () => {
+    it('Should Logout from the current user.', (done) => {
+      login.logoutUser(done);
     });
+  });
 
-    describe('Pausing', () => {
-      it('Should pause the test for 5 seconds', (done) => {
-        common.pause(done, 5000);
-      });
+  describe('Pausing.', () => {
+    it('Should pause the test for 2 seconds.', (done) => {
+      common.pause(done, 2000);
     });
-
-    describe('Logout', () => {
-      it('Should Logout from the current user', (done) => {
-        login.logoutUser(done);
-      });
-    });
-
-    describe('Pausing', () => {
-      it('Should pause the test for 2 seconds', (done) => {
-        common.pause(done, 2000);
-      });
-    });
+  });
 
 })
